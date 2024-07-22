a12c8476aa35a8153ee6262745deb7a87144e4f9
Rutvikaa06 <rutvika.munde06@gmail.com>
2024-07-22 17:26:17 +0530

    Add files via upload

---
+++ wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for corona_services_and_updates project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'corona_services_and_updates.settings')
+
+application = get_wsgi_application()
